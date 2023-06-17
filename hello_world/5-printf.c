6f5e1213ecc4200006a89c17be956b062ea83bf9
KhairiTab <taboubikhairi68@gmail.com>
2023-06-17 12:04:15 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stiod.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	printf("with proper grammar,but the outcomeis a piece of art,");
+		return (0);
+}
