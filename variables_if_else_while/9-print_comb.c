97128029ff8e7207879f0e643d56fbb8417856db
KhairiTab <taboubikhairi68@gmail.com>
2023-06-21 02:56:13 -0700

    mmm

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,13 +2,14 @@
 int main(void)
 {
 int i;
-for (i = 48; i < 57; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
+if (i != 57)
+{
 putchar(44);
 putchar(32);
 }
-putchar(57);
-
+putchar('\n');
 return (0);
 }
