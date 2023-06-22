c9e8666ebc6fdc80b22c58a394df021fa4c1ffdf
KhairiTab <taboubikhairi68@gmail.com>
2023-06-22 04:50:49 -0700

    1-

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,11 +2,11 @@
 /**
  * function that prints the alphabet, in lowercase
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a'; i <= 'z; i++)
 	{
 		_putchar(i);
 	}
