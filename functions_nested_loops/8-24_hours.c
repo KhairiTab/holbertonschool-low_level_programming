74493f131d40d1a1f2ef5682ee5bc0081835366c
KhairiTab <taboubikhairi68@gmail.com>
2023-06-23 03:37:35 -0700

    -8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return
+ */
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 48; a <= 50; a++)
 	{
 		for (b = 48; b <= 57; b++)
@@ -9,16 +15,16 @@ void jack_bauer(void)
 			for (c = 48; c <= 53; c++)
 			{
 				for (d = 48; d <= 57; d++)
-					if (a = 50 && b = 52)
-					{
+				{
+					if (a >= 50 && b >= 52)
 						break;
 					_putchar(a);
-				        -putchar(b);
-				        _putchar(58);
-				        _putchar(c);
-				        _putchar(d);
-				        _putchar('\n');
-					}
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
 			}
 		}
 	}
