7eaf257a4575f72ed7acf0c4186b162ac2b8579f
KhairiTab <taboubikhairi68@gmail.com>
2023-06-23 05:04:31 -0700

    -11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (i = n;i <= 98;i++)
+		{
+		printf("%d, ",i);
+		}
+	}
+	else 
+	{
+		if (n > 98)
+		{
+		       for (i = n;i >= 98; i--)
+		       printf("%d, ",i);
+		}
+	}
+	printf("%d, ",n);
+}
