502620950a2a51a25ca6450d96cc34158a726d1e
KhairiTab <taboubikhairi68@gmail.com>
2023-06-23 10:49:03 -0700

    101

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,s3,s5,s;
+	i=1;
+	do
+	{
+		s3=(3*i);
+                s5=(5*i);
+		printf("%d,%d",s3,s5);
+		s=s3+s5;
+		i=i++;
+	}
+	while(s<=1024);
+}
