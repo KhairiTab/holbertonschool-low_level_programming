2617de19372121685d8c22737a7db2d9ff8dee58
KhairiTab <taboubikhairi68@gmail.com>
2023-06-25 08:09:25 -0700

    -0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,5 @@
 #include "main.h"
 int _isupper(int c)
 {
-if (c > 65 && c < 91)
-{
-	return (1);
-}
-	else
-           if (c > 97 && c < 123)
-{
-	return (0);
-}
+	return (c >= 'A' && c <= 'Z');
 }
