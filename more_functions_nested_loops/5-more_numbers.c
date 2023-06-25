732de58fc5383e3db54ec8c581a83a6685bf300f
KhairiTab <taboubikhairi68@gmail.com>
2023-06-25 09:28:11 -0700

    -5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,10 @@
 #include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
 void more_numbers(void)
 {
-	
 	int i, j;
 
 	for (i = 0; i < 10; i++)
@@ -14,5 +17,4 @@ void more_numbers(void)
 		}
 		_putchar('\n');
 	}
-
 }
