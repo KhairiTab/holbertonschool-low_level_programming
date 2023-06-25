3b9b999f2a5c8b0a6bded4fbb3d047842d0e0f6c
KhairiTab <taboubikhairi68@gmail.com>
2023-06-25 10:09:57 -0700

    -7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	i = 0;
+	else
+	{
+		do 
+		{
+			_putchar('\');
+			_putchar('\n');
+			i = i + 1;
+		}
+		while (i = n);
+		_putchar('\n');
+}
