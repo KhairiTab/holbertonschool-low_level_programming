f92b7cc037bc27a61337215cb601944bded35cb7
KhairiTab <taboubikhairi68@gmail.com>
2023-07-02 11:05:41 -0700

    main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,4 +31,5 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *_strstr(char *haystack, char *needle);
 #endif
