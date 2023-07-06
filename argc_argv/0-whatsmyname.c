d5e28ab12295287070ef0a5047e77f38c2d27694
KhairiTab <taboubikhairi68@gmail.com>
2023-07-06 03:13:17 -0700

    test

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+/**
+ * main - print the program name
+ *
+ * @argc: count of arg
+ *
+ * @argv: vector of arg
+ *
+ * Return: 0
+ */
+
 int main(int __attribute__ ((unused)) argc, char **argv)
 {
-	printf("%s",*argv);
+	printf("%s", *argv);
 	printf("\n");
 	return (0);
 }
