27dbb072a2fdf0aa7b6a0b722b1efcc362003a73
KhairiTab <taboubikhairi68@gmail.com>
2023-07-06 04:30:39 -0700

    test

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	printf("error");
+	else
+	{
+	printf("%d", atoi(argv[1]) * atoi(argv[2]));
+	printf("\n");
+	}
+return (0);
+}
