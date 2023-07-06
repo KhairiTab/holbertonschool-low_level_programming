80cee86f1a16bf929eec3aa11abd09278f28413c
KhairiTab <taboubikhairi68@gmail.com>
2023-07-06 03:42:13 -0700

    1-args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print number of arg
+ * @argc: number of argc
+ * @argv: array of arg
+ * Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) **argv)
+{
+	printf("%d", argc);
+	printf("\n");
+return (0);
+}
