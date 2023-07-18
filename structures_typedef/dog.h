7d94115c0586da616e7f68510a2c7830aabacd91
KhairiTab <taboubikhairi68@gmail.com>
2023-07-18 17:08:02 +0200

    test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
@@ -16,4 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
