74567e7b457ee64cdcd729f80faae1e4d29650b4
KhairiTab <taboubikhairi68@gmail.com>
2023-07-21 17:40:31 +0200

    test

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- * Return: no return.
+ * print_name - print the name
+ * @name: name to print
+ * @f: function to use
  */
 
 void print_name(char *name, void (*f)(char *))
