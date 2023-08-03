ed9f307cd241c545412c34b6b0a7706a17014359
KhairiTab <taboubikhairi68@gmail.com>
2023-08-03 12:49:16 +0100

    test

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,19 +5,17 @@
  * Return: number of elements in the list.
  */
 
-size_t print_list(const list_t *h)
-{
+size_t print_list(const list_t *h){
 	size_t count;
-
 	count = 0;
 	while (h != NULL)
-	{
+    {
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
+    count++;
 	}
 	return (count);
 }
