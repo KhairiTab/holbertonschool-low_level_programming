638cc9ccc6b862096f646498c06edccf7ca3483b
KhairiTab <taboubikhairi68@gmail.com>
2023-08-03 17:47:54 +0100

    test

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,38 +1,39 @@
 #include <stdlib.h>
 #include <string.h>
-/**
- * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
- */
 
-list_t *add_node_end(list_t **head, const char *str)
-{
-    
+list_t *add_node_end(list_t **head, const char *str) {
+    // Allocate memory for the new node
     list_t *new_node = (list_t *)malloc(sizeof(list_t));
-    
-    if (new_node == NULL)
-    return NULL;
+    if (new_node == NULL) {
+        // Memory allocation failed
+        return NULL;
+    }
+
+    // Duplicate the input string
     new_node->str = strdup(str);
     if (new_node->str == NULL) {
+        // Memory allocation for the string failed
         free(new_node);
         return NULL;
     }
+
+    // Calculate the length of the new string
     new_node->len = strlen(str);
+
+    // Set the next pointer of the new node to NULL as it will be the last node
     new_node->next = NULL;
-    if (*head == NULL)
-     {
-    *head = new_node;
-    }
-     else {
+
+    // If the list is empty, set the new node as the head
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        // Traverse to the last node and update its next pointer to the new node
         list_t *current = *head;
-        while (current->next != NULL)
-        {
+        while (current->next != NULL) {
             current = current->next;
         }
         current->next = new_node;
     }
+
     return new_node;
 }
