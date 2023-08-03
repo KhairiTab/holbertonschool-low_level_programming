bd5534a0bbf0a5bd1b8e6785899391595dd42996
KhairiTab <taboubikhairi68@gmail.com>
2023-08-03 14:19:41 +0100

    test

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list.
@@ -6,22 +7,22 @@
  * @str: string to store in the list.
  * Return: address of the head.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node = (list_t *)malloc(sizeof(list_t));
+    list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
-if (new_node == NULL)
-return NULL;
+    if (new_node == NULL)
+        return (NULL);
 
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return NULL;
-}
-new_node->len = strlen(str);
-new_node->next = *head;
-*head = new_node;
-return (*head);
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+    return (*head);
 }
