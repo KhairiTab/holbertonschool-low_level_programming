002390f73871b8c63567de746a99f986db9f1fc6
KhairiTab <taboubikhairi68@gmail.com>
2023-08-03 17:50:36 +0100

    test

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-list_t *add_node_end(list_t **head, const char *str) {
-    // Allocate memory for the new node
+list_t *add_node_end(list_t **head, const char *str) 
+{
     list_t *new_node = (list_t *)malloc(sizeof(list_t));
-    if (new_node == NULL) {
-        // Memory allocation failed
+    if (new_node == NULL) 
+    {
         return NULL;
     }
 
-    // Duplicate the input string
     new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        // Memory allocation for the string failed
+    if (new_node->str == NULL) 
+    {
         free(new_node);
         return NULL;
     }
 
-    // Calculate the length of the new string
     new_node->len = strlen(str);
 
-    // Set the next pointer of the new node to NULL as it will be the last node
     new_node->next = NULL;
 
-    // If the list is empty, set the new node as the head
-    if (*head == NULL) {
+    if (*head == NULL)
+    {
         *head = new_node;
-    } else {
-        // Traverse to the last node and update its next pointer to the new node
+    }
+     else
+    {
         list_t *current = *head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
+        {
             current = current->next;
         }
         current->next = new_node;
     }
 
     return new_node;
-}
+    }
+    
