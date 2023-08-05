a3e8be508dbbb370c52df09d677dc2322972e01a
KhairiTab <taboubikhairi68@gmail.com>
2023-08-05 13:33:09 +0100

    modif

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
-#include"main.h"
+#include "holberton.h"
+
 /**
  * binary_to_uint - converts a binary number to an
  * unsigned int.
@@ -6,21 +7,31 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b) 
+unsigned int binary_to_uint(const char *b)
 {
-    if (b == NULL)
-    return 0;
-    int result = 0;
-    for (int i = 0; b[i] != '\0'; i++)
-    {
-    if (b[i] == '0' || b[i] == '1')
-    {
-    result = result * 2 + (b[i] - '0');
-    }
-    else
-    {
-    return 0;
-    }
-    }
-    return (result);
+	unsigned int ui;
+	int i, j;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[i] != '\0'; i++)
+		;
+
+	for (i--, j = 1; i >= 0; i--, j *= 2)
+	{
+		if (b[i] != '0' && b[j] != '1')
+		{
+			return (0);
+		}
+
+		if (b[i] & 1)
+		{
+			ui += j;
+		}
+	}
+
+	return (ui);
 }
