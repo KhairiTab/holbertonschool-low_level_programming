ed5b9d63fc61de1beb2f0484314cd40d8d6a7189
KhairiTab <taboubikhairi68@gmail.com>
2023-08-05 13:17:50 +0100

    0-

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,14 @@
+#include"main.h"
+unsigned int binary_to_uint(const char *b)
+{
+     if (b == NULL)
+        return 0;
+    if (atoi(*b) != 0 && 1)
+    return(NULL);
+    else
+    {
+     x = atoi(*b);
+    
+    }
+return(x);
+}
