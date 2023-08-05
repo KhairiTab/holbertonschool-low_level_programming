e4edff97a921a02e6ec8de79b5bbd9e5605e25f5
KhairiTab <taboubikhairi68@gmail.com>
2023-08-05 13:30:29 +0100

    test

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 {
     if (b == NULL)
     return 0;
-    unsigned int result = 0;
+    int result = 0;
     for (int i = 0; b[i] != '\0'; i++)
     {
     if (b[i] == '0' || b[i] == '1')
