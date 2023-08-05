6148f26c08ef0bb6d8696f94fbc911cb89b98ace
KhairiTab <taboubikhairi68@gmail.com>
2023-08-05 13:29:48 +0100

    test

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,26 @@
 #include"main.h"
-unsigned int binary_to_uint(const char *b)
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b) 
 {
-     if (b == NULL)
-        return 0;
-    if (atoi(*b) != 0 && 1)
-    return(NULL);
+    if (b == NULL)
+    return 0;
+    unsigned int result = 0;
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+    if (b[i] == '0' || b[i] == '1')
+    {
+    result = result * 2 + (b[i] - '0');
+    }
     else
     {
-     x = atoi(*b);
-    
+    return 0;
     }
-return(x);
+    }
+    return (result);
 }
