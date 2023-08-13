149f86a8a9d89615a4138d385084209823b84532
KhairiTab <taboubikhairi68@gmail.com>
2023-08-13 20:25:29 +0100

    0-

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,5 +2,17 @@
 #define _hash_tables_H_
 #include<stdio.h>
 
+typedef struct hash_node_s
+{
+     char *key;
+     char *value;
+     struct hash_node_s *next;
+} hash_node_t;
+
+typedef struct hash_table_s
+{
+     unsigned long int size;
+     hash_node_t **array;
+} hash_table_t;
 
 #endif
