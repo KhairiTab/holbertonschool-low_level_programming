4b359f65b4e3f7e5694fdaf82de88b6afb3426d6
KhairiTab <taboubikhairi68@gmail.com>
2023-08-13 20:30:13 +0100

    test

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,10 +1,9 @@
 #ifndef _hash_tables_H_
 #define _hash_tables_H_
+
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 typedef struct hash_node_s
 {
      char *key;
@@ -17,7 +16,5 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
-
 hash_table_t *hash_table_create(unsigned long int size);
-
 #endif
